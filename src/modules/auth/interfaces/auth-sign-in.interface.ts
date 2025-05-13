5fcb6608189d1f0121c8554aee7680cfcad2df8d
mch-fauzy <mucha.fauzy@gmail.com>
2025-05-13 21:00:31 +0700

    fix: remove auth sign in interface

--- src/modules/auth/interfaces/auth-sign-in.interface.ts
+++
@@ -1,7 +0,0 @@
-import { UserInterface } from 'src/modules/user/interfaces/user.interface';
-
-// TODO: tobe removed what for ai claude
-export interface AuthSignInInterface {
-  token: string;
-  user: UserInterface;
-}
